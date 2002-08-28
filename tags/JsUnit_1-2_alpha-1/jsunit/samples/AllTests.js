8a8cb2cd98252a75cf748b921e6f7ba6a7b9f709
(no author) <(no author)@d4f5e813-a214-0410-99c4-8f31caba3383>
2002-08-28 19:52:36 +0000

    This commit was manufactured by cvs2svn to create tag 'JsUnit_1-2_alpha-1'.

---
+++ tags/JsUnit_1-2_alpha-1/jsunit/samples/AllTests.js
@@ -0,0 +1,61 @@
+/*
+JsUnit - a JUnit port for JavaScript
+Copyright (C) 1999,2000,2001,2002 Joerg Schaible
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation in version 2 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+Test suites built with JsUnit are derivative works derived from tested 
+classes and functions in their production; they are not affected by this 
+license.
+*/
+
+if( this.WScript )
+{
+	var fso = new ActiveXObject( "Scripting.FileSystemObject" );
+	var file = fso.OpenTextFile( "../lib/JsUtil.js", 1 );
+	var all = file.ReadAll();
+	file.Close();
+	eval( all );
+}
+else
+	load( "../lib/JsUtil.js" );
+
+eval( JsUtil.prototype.load( "../lib/JsUnit.js" ));
+eval( JsUtil.prototype.load( "ArrayTest.js" ));
+eval( JsUtil.prototype.load( "money/IMoney.js" ));
+eval( JsUtil.prototype.load( "money/Money.js" ));
+eval( JsUtil.prototype.load( "money/MoneyBag.js" ));
+eval( JsUtil.prototype.load( "money/MoneyTest.js" ));
+eval( JsUtil.prototype.load( "SimpleTest.js" ));
+
+function main( test )
+{
+	var runner = new TextTestRunner();
+	runner.addSuite( new ArrayTestSuite());
+	runner.addSuite( new MoneyTestSuite());
+	runner.addSuite( new SimpleTestSuite());
+	return runner.start( test );
+}
+
+var args;
+if( this.WScript )
+{
+	if( WScript.Arguments.Count())
+		args = WScript.Arguments( 0 );
+}
+else 
+	args = arguments;
+		
+JsUtil.prototype.quit( main( args ));
+
---
+++ tags/JsUnit_1-2_alpha-1/jsunit/tests/AllTests.js
@@ -0,0 +1,89 @@
+/*
+JsUnit - a JUnit port for JavaScript
+Copyright (C) 1999,2000,2001,2002 Joerg Schaible
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation in version 2 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+Test suites built with JsUnit are derivative works derived from tested 
+classes and functions in their production; they are not affected by this 
+license.
+*/
+
+var hasExceptions = "false";
+var exceptionsWorking = "false";
+
+function throwEx()
+{
+	throw new Object();
+}
+function testEx()
+{
+	var me = this;
+	try { hasExceptions = "true"; new throwEx(); } 
+	catch( ex ) { exceptionsWorking = this == me; }
+}
+new testEx();
+
+if( this.WScript )
+{
+	var fso = new ActiveXObject( "Scripting.FileSystemObject" );
+	var file = fso.OpenTextFile( "../lib/JsUtil.js", 1 );
+	var all = file.ReadAll();
+	file.Close();
+	eval( all );
+}
+else
+	load( "../lib/JsUtil.js" );
+
+JsUtil.prototype.print( "\nJavaScript compatibility:" );
+JsUtil.prototype.print( "\thas exceptions: " + hasExceptions );
+JsUtil.prototype.print( "\texceptions working: " + exceptionsWorking );
+
+JsUtil.prototype.print( "\nJavaScript engine detection:" );
+for( var i in JsUtil.prototype )
+	if( typeof JsUtil.prototype[i] != "function" && i.indexOf( "is" ) == 0 )
+		JsUtil.prototype.print( "\t" + i + ": " + JsUtil.prototype[i] );
+
+function main( args )
+{
+	var runner = new TextTestRunner();
+	runner.addSuite( new JsUtilTestSuite());
+	runner.addSuite( new JsUnitTestSuite());
+	return runner.start( args );
+}
+
+JsUtil.prototype.print( "\nJsUnit Test Suite:\n" );
+if( exceptionsWorking )
+{
+	eval( JsUtil.prototype.load( "../lib/JsUnit.js" ));
+	eval( JsUtil.prototype.load( "JsUtilTest.js" ));
+	eval( JsUtil.prototype.load( "JsUnitTest.js" ));
+
+	var args;
+	if( this.WScript )
+	{
+		if( WScript.Arguments.Count())
+			args = WScript.Arguments( 0 );
+	}
+	else 
+		args = arguments;
+		
+	JsUtil.prototype.quit( main( args ));
+}
+else
+{
+	JsUtil.prototype.print( "\tSorry, exceptions not working!\n" );
+	JsUtil.prototype.quit( -1 );
+}
+
